3f88b6e2261aff82fcd0a57902843328faedfe74
Daniel Jaroslawicz <daniel.jaroslawicz@gmail.com>
2020-04-08 18:10:06 -0400

    revised, but untested, data setup script

--- organize_data.py
+++ organize_data.py
@@ -19,29 +19,48 @@ Split images into train and test set based on a precomputed split
 """
 def organize_data():
     image_split = {}
-    with open('CUB_200_2011/train_test_split.txt', 'r') as split_file:
+    with open('train_test_split.txt', 'r') as split_file:
         for line in split_file:
             line = line.strip()
             image_id, is_train = line.split()
             image_split[image_id] = is_train
 
-    with open('CUB_200_2011/images.txt', 'r') as images_file:
+    with open('images.txt', 'r') as images_file:
         images = images_file.readlines()
 
     train_file = open('train.txt', 'w')
     test_file = open('test.txt', 'w')
 
+"""
+This snippet builds directory structure according to README but code doesn't actually expect that
+"""
+    # for image in images:
+    #     image_id, image_name = image.split()
+    #     # If the precomputed split says this is a train image move it to train directory
+    #     # and write file name and fake cluster to train.txt
+    #     if int(image_split[image_id]) == 1:
+    #         os.makedirs(os.path.dirname('train/' + image_name), exist_ok=True)
+    #         os.rename('images/' + image_name, 'train/' + image_name)
+    #         train_file.write(image_name + ' 0\n')
+    #     else:
+    #         os.makedirs(os.path.dirname('test/' + image_name), exist_ok=True)
+    #         os.rename('images/' + image_name, 'test/' + image_name)
+    #         test_file.write(image_name + ' 0\n')
+
+"""
+This snippet builds directory structure that seems to work (untested, needs to be confirmed)
+"""
     for image in images:
         image_id, image_name = image.split()
-        # If the precomputed split says this is a train image move it to train directory
-        # and write file name and fake cluster to train.txt
+        # Make the directory for this class
+        os.makedirs(os.path.dirname('train/' + image_name), exist_ok=True)
+        # Move the image to proper location
+        os.rename('images/' + image_name, './' + image_name)
+
+        # If the precomputed split says this is a train image write file name and fake cluster to train.txt
         if int(image_split[image_id]) == 1:
-            os.makedirs(os.path.dirname('train/' + image_name), exist_ok=True)
-            os.rename('CUB_200_2011/images/' + image_name, 'train/' + image_name)
             train_file.write(image_name + ' 0\n')
         else:
-            os.makedirs(os.path.dirname('test/' + image_name), exist_ok=True)
-            os.rename('CUB_200_2011/images/' + image_name, 'test/' + image_name)
             test_file.write(image_name + ' 0\n')
 
 
